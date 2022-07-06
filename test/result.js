24a26ab72c3c2c6720ef9ca4fe3590023bb5348a
padhaiskool <padhaiskool@gmail.com>
2022-07-06 16:34:03 +0530

    result page update commit

--- test/result.js
+++ test/result.js
@@ -9,5 +9,5 @@ let percentage = document.getElementById('percentage');
 
 right.innerHTML = v ;
 // wrong.innerHTML = t - v;
-total.innerHTML = t;
-percentage.innerHTML =Math.round(v*100/t)+"%";
+total.innerHTML = t-1;
+percentage.innerHTML =Math.round(v*100/t-1)+"%";
