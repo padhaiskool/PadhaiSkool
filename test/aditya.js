d86068baff20ca95dc53a08692b55c6915482831
padhaiskool <padhaiskool@gmail.com>
2022-07-06 16:17:50 +0530

    second commit

--- test/aditya.js
+++ test/aditya.js
@@ -1,206 +1,421 @@
+// const data = [
+//     {
+//         id:"1",
+//         question:"TO place the image into a HTML file ...... attribute is used in IMG tag",
+//         option1:"URL",
+//         option2:"ALT",
+//         option3:"SRC",
+//         option4:"HREF",
+//         ansid:"3"
+//     }
+//     ,{
+//         id:"2",
+//         question:"The elements of a record are.......",
+//         option1:"Homogeneous",
+//         option2:"Similar",
+//         option3:"Non-Homogeneous",
+//         option4:"Identical",
+//         ansid:"3"
+//     },
+//     {
+//         id:"3",
+//         question:"Windows NT is ....... operating system.",
+//         option1:"Multiuser",
+//         option2:"Multitasking",
+//         option3:"Multithreading",
+//         option4:"All of the above",
+//         ansid:"4"
+//     },
+//     {
+//         id:"4",
+//         question:"The ability to take more than one form is called ...... in object oriented programming.",
+//         option1:"Inheritance",
+//         option2:"Encapsulation",
+//         option3:"Polymorphism",
+//         option4:"Data abstraction",
+//         ansid:"3"
+//     },{
+//         id:"5",
+//         question:"Is not a operating system ?",
+//         option1:"UNIX",
+//         option2:"LINUX",
+//         option3:"MS-DOS",
+//         option4:"C++",
+//         ansid:"4"
+//     },{
+//         id:"6",
+//         question:"The most efficient search algorithm.",
+//         option1:"Binary search",
+//         option2:"Reverse search",
+//         option3:"Linear search",
+//         option4:"Pointer search",
+//         ansid:"1"
+//     },{
+//         id:"7",
+//         question:"VBScript can be executed in ..... web browser",
+//         option1:"Netscape navigator",
+//         option2:"Internet explorer",
+//         option3:"Both",
+//         option4:"None of these",
+//         ansid:"4"
+//     },{
+//         id:"8",
+//         question:"Bulleted list in HTML is created by ..... tag.",
+//         option1:"<UL>",
+//         option2:"<OL>",
+//         option3:"<B>",
+//         option4:"<Br>",
+//         ansid:"1"
+//     },{
+//         id:"9",
+//         question:"..... is very useful in situation when data is to be stored and retrieved in reverse order.",
+//         option1:"Stack",
+//         option2:"Queue",
+//         option3:"Linked List",
+//         option4:"Tree",
+//         ansid:"1"
+//     },{
+//         id:"10",
+//         question:"What will be the value of x after execution of following expression in c++? X= ++m+n+ +; where m=10 and n=15.",
+//         option1:"25",
+//         option2:"27",
+//         option3:"26",
+//         option4:"28",
+//         ansid:"3"
+//     },{
+//         id:"11",
+//         question:"...... is free softwear  (2019)",
+//         option1:"UNIX",
+//         option2:"WINDOWS",
+//         option3:"LINUX",
+//         option4:"DOS",
+//         ansid:"3"
+//     },{
+//         id:"12",
+//         question:"Turning attentions of CPU from one processor to another is called as ........",
+//         option1:"Reference",
+//         option2:"Context Switching",
+//         option3:"process Scheduling",
+//         option4:"Multithreading",
+//         ansid:"2"
+//     },{
+//         id:"13",
+//         question:"If lower bound = 0 and upperbound = 15 then midterm is ..... in binary search method.",
+//         option1:"6",
+//         option2:"7",
+//         option3:"8",
+//         option4:"9",
+//         ansid:"3"
+//     },{
+//         id:"14",
+//         question:"..... system is used to declare destructor function in c++.",
+//         option1:"@",
+//         option2:"#",
+//         option3:"~",
+//         option4:"!",
+//         ansid:"3"
+//     },{
+//         id:"15",
+//         question:".... tag is uses to write the defination list?",
+//         option1:"<ul>",
+//         option2:"<dl>",
+//         option3:"<ol>",
+//         option4:"<dl>",
+//         ansid:"2"
+//     },{
+//         id:"16",
+//         question:"Data items that are divided into subitems is called is ........ ",
+//         option1:"Group items",
+//         option2:"Elementary items",
+//         option3:"Nodes",
+//         option4:"Array",
+//         ansid:"1"
+//     },{
+//         id:"17",
+//         question:"Object is a .........",
+//         option1:"Variables",
+//         option2:"Data Types",
+//         option3:"Run time Entity",
+//         option4:"None of these",
+//         ansid:"3"
+//     },{
+//         id:"18",
+//         question:"In HTML for Red color , RGB code is .........",
+//         option1:"#000000",
+//         option2:"#ff0000",
+//         option3:"#00ff00",
+//         option4:"#0000ff",
+//         ansid:"3"
+//     },{
+//         id:"19",
+//         question:"...... is the only non-linear data structure from the following list .",
+//         option1:"Array",
+//         option2:"Linked list",
+//         option3:"Stack",
+//         option4:"Tree",
+//         ansid:"4"
+//     },{
+//         id:"20",
+//         question:"The attributes BORDER in <TABLE> tag has the default value of ........",
+//         option1:"2",
+//         option2:"1",
+//         option3:"0",
+//         option4:"None of These",
+//         ansid:"2"
+//     },{
+//         id:"21",
+//         question:"Ability take more than one form is called ......",
+//         option1:"Class",
+//         option2:"Polymorphism",
+//         option3:"Inheritance",
+//         option4:"Overloading",
+//         ansid:"2"
+//     },{
+//         id:"22",
+//         question:"...... are the operating system programs.",
+//         option1:"User Program",
+//         option2:"Anti-virus programs",
+//         option3:"Process Management programs",
+//         option4:"#00 00 ff",
+//         ansid:"3"
+//     },{
+//         id:"23",
+//         question:"If a complete binary tree (tn) has n =100 nodes , then its depth (dn) is .........",
+//         option1:"10",
+//         option2:"20",
+//         option3:"50",
+//         option4:"100",
+//         ansid:"1"
+//     },{
+//         id:"24",
+//         question:"The size of GIF format file is .......",
+//         option1:"Greater than BMP format file ",
+//         option2:"Less than BMF format file",
+//         option3:"Equal to BMF format file",
+//         option4:"Greater than JPEG file",
+//         ansid:"2"
+//     },{
+//         id:"25",
+//         question:"Context switching is the term related to ......... management.",
+//         option1:"Process",
+//         option2:"Memory",
+//         option3:"Information",
+//         option4:"Device",
+//         ansid:"1"
+//     },{
+//         id:"26",
+//         question:"You Can Submit Your Test!",
+//         option1:"",
+//         option2:"",
+//         option3:"",
+//         option4:"",
+//         ansid:"5"
+//     }
+// ];
+
 const data = [
     {
         id:"1",
-        question:"TO place the image into a HTML file ...... attribute is used in IMG tag",
-        option1:"URL",
-        option2:"ALT",
-        option3:"SRC",
-        option4:"HREF",
-        ansid:"3"
+        question:"The number of σ and π bonds in 2-formylbenzoic acid are respectively",
+        option1:" 10,3",
+        option2:"14,3",
+        option3:"12,5",
+        option4:"17,5",
+        ansid:"4"
     }
     ,{
         id:"2",
-        question:"The elements of a record are.......",
-        option1:"Homogeneous",
-        option2:"Similar",
-        option3:"Non-Homogeneous",
-        option4:"Identical",
-        ansid:"3"
+        question:"The volume of 1 mole of any pure gas at standard temperature and pressure is always equal to",
+        option1:" 0.022414 m3        ",
+        option2:"22.414 m3",
+        option3:"2.2414 m3",
+        option4:"0.2414 m3",
+        ansid:"1"
     },
     {
         id:"3",
-        question:"Windows NT is ....... operating system.",
-        option1:"Multiuser",
-        option2:"Multitasking",
-        option3:"Multithreading",
-        option4:"All of the above",
-        ansid:"4"
+        question:"Veronal is used as a/an",
+        option1:"analgesic",
+        option2:"tranquilizer",
+        option3:"antihistamine",
+        option4:"antibiotic",
+        ansid:"2"
     },
     {
         id:"4",
-        question:"The ability to take more than one form is called ...... in object oriented programming.",
-        option1:"Inheritance",
-        option2:"Encapsulation",
-        option3:"Polymorphism",
-        option4:"Data abstraction",
-        ansid:"3"
+        question:" The oxidation number of sulphur in S8 molecule is",
+        option1:"6",
+        option2:"0",
+        option3:"2",
+        option4:"8",
+        ansid:"2"
     },{
         id:"5",
-        question:"Is not a operating system ?",
-        option1:"UNIX",
-        option2:"LINUX",
-        option3:"MS-DOS",
-        option4:"C++",
-        ansid:"4"
+        question:"Which of the following is also called as nitrogen sesquioxide",
+        option1:"NO2",
+        option2:"N2O3",
+        option3:"N2O4",
+        option4:"N2O5",
+        ansid:"2"
     },{
         id:"6",
-        question:"The most efficient search algorithm.",
-        option1:"Binary search",
-        option2:"Reverse search",
-        option3:"Linear search",
-        option4:"Pointer search",
-        ansid:"1"
-    },{
-        id:"7",
-        question:"VBScript can be executed in ..... web browser",
-        option1:"Netscape navigator",
-        option2:"Internet explorer",
-        option3:"Both",
-        option4:"None of these",
+        question:"Which among the following is a set of nucleophiles ",
+        option1:" H+        , NH3        , Cl-",
+        option2:" BF3        , H2O , NH3",
+        option3:"AlCl3        , BF3        ,NH3       ",
+        option4:"CN-       , H2O , R-OH",
         ansid:"4"
     },{
+        id:"7",
+        question:" Which of the following acts as oxidising agent in hydrogen - oxygen fuel cell",
+        option1:"H2",
+        option2:"O2",
+        option3:"Both",
+        option4:"C",
+        ansid:"2"
+    },{
         id:"8",
-        question:"Bulleted list in HTML is created by ..... tag.",
-        option1:"<UL>",
-        option2:"<OL>",
-        option3:"<B>",
-        option4:"<Br>",
-        ansid:"1"
+        question:" In ozone molecule , the formal charge on the central oxygen atom is        ",
+        option1:"-1",
+        option2:"+2",
+        option3:"0",
+        option4:"+1",
+        ansid:"4"
     },{
         id:"9",
-        question:"..... is very useful in situation when data is to be stored and retrieved in reverse order.",
-        option1:"Stack",
-        option2:"Queue",
-        option3:"Linked List",
-        option4:"Tree",
-        ansid:"1"
-    },{
-        id:"10",
-        question:"What will be the value of x after execution of following expression in c++? X= ++m+n+ +; where m=10 and n=15.",
-        option1:"25",
-        option2:"27",
-        option3:"26",
-        option4:"28",
+        question:" According to Werners theory , the geometry of the complex is determined by        ",
+        option1:" only from the primary valence in space",
+        option2:"number and position of the primary valences in space        ",
+        option3:"number and position of the secondary valences in space        ",
+        option4:"only from the position of secondary valence in space        ",
         ansid:"3"
     },{
+        id:"10",
+        question:"How many total constituent particles are present in simple cubic unit cell ?",
+        option1:"1",
+        option2:"2",
+        option3:"3",
+        option4:"4",
+        ansid:"1"
+    },{
         id:"11",
-        question:"...... is free softwear  (2019)",
-        option1:"UNIX",
-        option2:"WINDOWS",
-        option3:"LINUX",
-        option4:"DOS",
+        question:"Which among the following is a neutral complex ? ",
+        option1:" [Fe(H2O)6]Cl3",
+        option2:"[Ni(NH3)6]Cl2",
+        option3:"[Pt(NH3)2Cl2]",
+        option4:"K[Ag(CN)2]",
         ansid:"3"
     },{
         id:"12",
-        question:"Turning attentions of CPU from one processor to another is called as ........",
-        option1:"Reference",
-        option2:"Context Switching",
-        option3:"process Scheduling",
-        option4:"Multithreading",
-        ansid:"2"
+        question:"Two moles of an ideal gas is expanded isothermally and reversibly at 300 K from 1L to 10 L .The enthalpy change in kJ is",
+        option1:"11.4 kJ        ",
+        option2:" 4.8 kJ",
+        option3:" -11.4 kJ        ",
+        option4:"ZERO KJ",
+        ansid:"4"
     },{
         id:"13",
-        question:"If lower bound = 0 and upperbound = 15 then midterm is ..... in binary search method.",
-        option1:"6",
-        option2:"7",
-        option3:"8",
-        option4:"9",
-        ansid:"3"
+        question:"        α-Chlorosodium acetate on boiling with aqueous sodium nitrite gives",
+        option1:"nitromethane",
+        option2:" α-chloronitromethane        ",
+        option3:"nitroethane",
+        option4:"acetyl chloride        ",
+        ansid:"1"
     },{
         id:"14",
-        question:"..... system is used to declare destructor function in c++.",
-        option1:"@",
-        option2:"#",
-        option3:"~",
-        option4:"!",
-        ansid:"3"
+        question:"The bond angle H-O-O in H2O2 in gaseous phase is",
+        option1:" 90.2        ",
+        option2:"111.5",
+        option3:"101.9",
+        option4:"94.8",
+        ansid:"4"
     },{
         id:"15",
-        question:".... tag is uses to write the defination list?",
-        option1:"<ul>",
-        option2:"<dl>",
-        option3:"<ol>",
-        option4:"<dl>",
+        question:"How many metameric ethers are represented by the molecular formula C4H10O ?        ",
+        option1:"4",
+        option2:"3",
+        option3:"2",
+        option4:"5",
         ansid:"2"
     },{
         id:"16",
-        question:"Data items that are divided into subitems is called is ........ ",
-        option1:"Group items",
-        option2:"Elementary items",
-        option3:"Nodes",
-        option4:"Array",
-        ansid:"1"
+        question:"Which of following metals occurs in native state ?",
+        option1:"Magnesium",
+        option2:"Platinum",
+        option3:"Potassium",
+        option4:"SODIUM",
+        ansid:"2"
     },{
         id:"17",
-        question:"Object is a .........",
-        option1:"Variables",
-        option2:"Data Types",
-        option3:"Run time Entity",
-        option4:"None of these",
-        ansid:"3"
+        question:"Which of the following is NOT a broadspectrum anitibiotics ?",
+        option1:"Penicillin",
+        option2:"Amoxicillin",
+        option3:"Chloramphenicol",
+        option4:"Ampicillin",
+        ansid:"1"
     },{
         id:"18",
-        question:"In HTML for Red color , RGB code is .........",
-        option1:"#000000",
-        option2:"#ff0000",
-        option3:"#00ff00",
-        option4:"#0000ff",
-        ansid:"3"
+        question:"The oxidation state of sulphur in H2S2O7 is",
+        option1:"+4",
+        option2:"+6",
+        option3:"+5",
+        option4:"+7",
+        ansid:"2"
     },{
         id:"19",
-        question:"...... is the only non-linear data structure from the following list .",
-        option1:"Array",
-        option2:"Linked list",
-        option3:"Stack",
-        option4:"Tree",
-        ansid:"4"
+        question:"The reaction in which 2 molecules of chlorobenzene reacts with metallic sodium in presence of dry ether forming diphenyl is an example of,",
+        option1:"Wurtz -Fittig reaction        ",
+        option2:"Wurtz reaction        ",
+        option3:"Rosenmund reaction        ",
+        option4:" Balz-Schiemann reaction",
+        ansid:"1"
     },{
         id:"20",
-        question:"The attributes BORDER in <TABLE> tag has the default value of ........",
-        option1:"2",
-        option2:"1",
-        option3:"0",
+        question:"The percentage of unoccupied volume in simple cubic cell is",
+        option1:"52.40%",
+        option2:"32.00%",
+        option3:"47.60%",
         option4:"None of These",
-        ansid:"2"
+        ansid:"3"
     },{
         id:"21",
-        question:"Ability take more than one form is called ......",
-        option1:"Class",
-        option2:"Polymorphism",
-        option3:"Inheritance",
-        option4:"Overloading",
-        ansid:"2"
+        question:"Isobutylene on hydroboration followed by oxidation with hydrogen peroxide in presence of base yields",
+        option1:" n-butyl alcohol        ",
+        option2:"sec-butyl alcohol",
+        option3:" tert-butyl alcohol",
+        option4:"isobutyl alcohol        ",
+        ansid:"4"
     },{
         id:"22",
-        question:"...... are the operating system programs.",
-        option1:"User Program",
-        option2:"Anti-virus programs",
-        option3:"Process Management programs",
-        option4:"#00 00 ff",
+        question:"what is the density of water vapour at boiling point of water ?",
+        option1:" 1 x 10-4 g cm-3        ",
+        option2:"1 g cm-3",
+        option3:" 6 x 10-4 g cm-3",
+        option4:" 4 x 10-4 g cm-3        ",
         ansid:"3"
     },{
         id:"23",
-        question:"If a complete binary tree (tn) has n =100 nodes , then its depth (dn) is .........",
-        option1:"10",
-        option2:"20",
-        option3:"50",
-        option4:"100",
+        question:"which of the following molecules form a zwitter ion ?",
+        option1:". H2NCH2COOH        ",
+        option2:". CH3COC2H5",
+        option3:" CH3CH2COOH",
+        option4:"CH3COOCH3",
         ansid:"1"
     },{
         id:"24",
-        question:"The size of GIF format file is .......",
-        option1:"Greater than BMP format file ",
-        option2:"Less than BMF format file",
-        option3:"Equal to BMF format file",
-        option4:"Greater than JPEG file",
+        question:"which reaction is useful in exchange of halogen in alkyl chloride by iodide ?",
+        option1:" Wurtz reaction        ",
+        option2:"Finkelstein reaction",
+        option3:" Reimer-Tiemann reaction",
+        option4:"Williamson synthesis",
         ansid:"2"
     },{
         id:"25",
-        question:"Context switching is the term related to ......... management.",
-        option1:"Process",
-        option2:"Memory",
-        option3:"Information",
-        option4:"Device",
+        question:"Propene when treated with cold conc. H2SO4 forms a compound which on heating with water gives",
+        option1:" propan-2-ol",
+        option2:". butan-1-ol",
+        option3:"ethanol",
+        option4:"propan-1-ol",
         ansid:"1"
     },{
         id:"26",
