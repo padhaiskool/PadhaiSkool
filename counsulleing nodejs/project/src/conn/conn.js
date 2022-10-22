45aef8d86ef498469dcac5f7f364a492b0a6e78f
padhaiskool <108663978+padhaiskool@users.noreply.github.com>
2022-10-22 17:11:22 +0530

    Delete counsulleing nodejs directory

--- counsulleing nodejs/project/src/conn/conn.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const db = 'mongodb+srv://adityasabde:aditya001@cluster0.zhgi0bu.mongodb.net/padhaiskool';
-
-mongoose.connect(db).then(()=>{
-    console.log("database connected.......");
-}).catch((e)=>{
-    console.log(`something went wrong ${e}`)
-});
